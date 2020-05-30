ae75d850d5e29d21729de862e7cde4a12e65e087
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2020-05-30 17:33:49 +0300

    code refactoring

--- random_walk.py
+++ random_walk.py
@@ -1,27 +1,27 @@
 from random import choice
 
 
+def get_step(direction_variants=[1, -1], way_length=[0, 1, 2, 3, 4]):
+    direction = choice(direction_variants)
+    distance = choice(way_length)
+    return direction*distance
+
+
 class RandomWalk:
 
     def __init__(self, num_points=5000):
         """Initializes random walk attributes"""
         self.num_points = num_points
-
         # random walk starts in the point with coordinates (0, 0)
         self.x_values = [0]
         self.y_values = [0]
-
 
     def fill_walk(self):
         """Calculation of the walking points"""
         while len(self.x_values) < self.num_points:
             # Determining the direction and length of movement
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = get_step()
+            y_step = get_step()
             # Reject zero moves
             if x_step == 0 and y_step == 0:
                 continue
@@ -30,3 +30,4 @@ class RandomWalk:
             next_y = self.y_values[-1] + y_step
             self.x_values.append(next_x)
             self.y_values.append(next_y)
+
