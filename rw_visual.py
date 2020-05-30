c3c2ea64a3314983b4a64547107f30328a5ee667
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2020-05-30 18:57:21 +0300

    subplots added

--- rw_visual.py
+++ rw_visual.py
@@ -2,19 +2,22 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(500)
     rw.fill_walk()
-    # View area setting
-    plt.figure(dpi=128, figsize=(10, 6))
+    fig, ax = plt.subplots(4, 1)
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=16)
+    ax[0].scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=16)
     # Highlighting of the first and last points
-    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    ax[0].scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax[0].scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    ax[0].set_title("Random walking plots")
+    ax[0].set_facecolor('black')
     # Hide axis
-    axes = plt.axes()
-    axes.get_xaxis().set_visible(False)
-    axes.get_yaxis().set_visible(False)
+    ax[0].get_xaxis().set_visible(False)
+    ax[0].get_yaxis().set_visible(False)
+    fig.set_figwidth(10)
+    fig.set_figheight(10)
+
     plt.show()
     keep_running = input("Another walk?(y/n):")
     if keep_running == 'n':
